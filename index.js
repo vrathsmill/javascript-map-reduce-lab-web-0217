355adc5ab2c4ae78c1b07513905c4d95ec54d9fa
vrathsmill <vlrathsmill@gmail.com>
2017-03-19 16:23:24 -0400

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,37 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+var issuesWithUpdatedApiUrl =
+ issues.map(function (issue) {
+    return Object.assign({}, issue, {
+      url: issue.url.replace('api.github.com', 'api-v2.github.com')
+    });
+  });
+
+  var commentCountAcrossIssues =
+    issues.map(issue =>
+      issue.comments_count).reduce((acc, val) =>
+        acc + val, 0)
+
+  var openIssues = issues.filter(issue => {
+      if (issue.state === 'open') {
+        return issue;
+      }
+    }
+  )
+
+  var nonAutomaticIssues =
+    issues.filter(issue => {
+      if (!issue.body.includes('automatically')) {
+      return issue
+      }
+    })
+
+  const body = document.getElementById('results');
+  body.innerHTML = nonAutomaticIssues
+    .map(issue => `<tr>
+      <td>${issue.body}</td>
+      <td>${issue.created_at}</td>
+      <td>${issue.state}</td>
+      </tr>`).join(' ');
